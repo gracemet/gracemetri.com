c1ff5e1a8ce7060e668d2d9c04549932a6f4b28e
Grace Metri <grace.metri@gmail.com>
2026-06-23 00:47:18 -0500

    Add footer and JS to copy number. Implement one main.css instead of individual CSS pages.

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
-function setCurrent(section) {
-    // Remove the 'current' class from all nav links
-    const navLinks = document.querySelectorAll('nav a');
-    navLinks.forEach(link => {
-        link.classList.remove('current');
-    });
+function copyToClipboard(info) {
+    navigator.clipboard.writeText(info)
 
-    // Add the 'current' class to the clicked link
-    const currentLink = document.querySelector(`nav a[href="#${section}"]`);
-    if (currentLink) {
-        currentLink.classList.add('current');
-    }
+    document.getElementById("phone").innerHTML = "Copied &checkmark;"
+    // document.getElementById("phone").style = ""
 
 }
 
